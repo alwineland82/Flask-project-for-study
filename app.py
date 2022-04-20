8b35c951b25823546c75fbb3fdbc5f10a2c3446d
al_wineland <alwineland@yandex.ru>
2022-04-20 20:19:22 +1100

    app.py jinja play

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template("index.html")
+@app.route('/<x>/')
+def index(x):
+    x = float(x)
+    pi = 3.14
+    return render_template("index.html", 
+                            text=f"Circle area = {x ** 2 * pi}")
