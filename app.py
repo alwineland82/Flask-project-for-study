75006c8d6274fa66e9662ecec77df1fea7290e83
al_wineland <alwineland@yandex.ru>
2022-05-03 03:21:21 +1100

    gameoflife' git commit -m gameoflife git commit -m "gameoflife

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template
-
+from game_of_life import *
 
 app = Flask(__name__)
 
-def boo(x, y):
-    return x + y
-
-
 @app.route('/')
 def index():
-    return render_template("index.html", boo=boo)
+    GameOfLife.counter = 0
+    GameOfLife(25, 15)
+    return render_template("index.html")
+
+
+@app.route('/live')
+def live():
+    game = GameOfLife(25, 15)
+    game.form_new_generation()
+    return render_template("live.html", game=game, len=len)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+    
